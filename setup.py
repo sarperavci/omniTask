b9be6eb5432fa0a7455f5be322f704f193b9fa17
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2025-05-25 22:13:23 +0300

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omniTask",
-    version="0.6.0",
+    version="0.7.0",
     packages=find_packages(),
     install_requires=[],
     author="SARPER AVCI",
