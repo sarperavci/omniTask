d5f34f9248162db0f72d4c4446105f508227b50f
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2025-07-14 20:45:41 +0300

    Bump version: 0.7.1 → 1.0.0

--- omniTask/__init__.py
+++ omniTask/__init__.py
@@ -6,4 +6,4 @@ from .models.task_group import TaskGroupConfig, TaskGroup, StreamingTaskGroup
 from .utils.logging import setup_task_logging, TaskLogFormatter
 from .utils.workflow_checker import WorkflowChecker 
 
-__version__ = "0.7.1"
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omniTask",
-    version="0.7.1",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[],
     author="SARPER AVCI",
